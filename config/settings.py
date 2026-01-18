b49ffaa767146022b79b76b155099f78167ccfd2
hermantoXYZ <andiupdates@gmail.com>
2026-01-19 00:11:01 +0800

    p

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static" / "assets",
+    os.path.join(BASE_DIR, 'static', 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default URL on which Django application runs for specific environment
